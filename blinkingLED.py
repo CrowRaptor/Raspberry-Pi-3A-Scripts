ea648121c36f5b6a69463a23a8d594328edeab31
Alina <alinaesmoore@gmail.com>
2023-12-25 18:44:14 +0000

    Update blinkingLED.py

--- blinkingLED.py
+++ blinkingLED.py
@@ -3,11 +3,11 @@ from time import sleep      #get sleep func from time module
 
 GPIO.setwarnings(False)     #ignore problems
 GPIO.setmode(GPIO.BOARD)       #use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)   #set pin 8 as output and set value to off
+GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW)   #set pin 8 as output and set value to off
 
 while True: #run forever
-    GPIO.output(8, GPIO.HIGH)   #turn on
+    GPIO.output(5, GPIO.HIGH)   #turn on
     sleep(1)    #1 second eep
-    GPIO.output(8, GPIO.LOW)
+    GPIO.output(5, GPIO.LOW)
     sleep(1)
     
